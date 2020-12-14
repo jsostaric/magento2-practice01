20ab4592939c647a9dcd4ce649a4088e923d01c6
jsostaric <bugificiran@gmail.com>
2020-12-14 15:21:42 +0100

    block follows config settings

--- Helloworld/Block/Hello.php
+++ Helloworld/Block/Hello.php
@@ -2,8 +2,23 @@
 
 namespace Inchoo\Helloworld\Block;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template;
+
 class Hello extends \Magento\Framework\View\Element\Template
 {
+    protected $scopeConfig;
+
+    public function __construct(Template\Context $context, array $data = [], ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
+    }
+
+    public function isVisible(): bool
+    {
+        return $this->scopeConfig->getValue('helloworld/general/enable');
+    }
     /**
      * @return string
      */
